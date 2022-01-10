94afeb6816aa3040f251196dbbd0d8d44e6966cc
Javier Alarcon <alarcon.vt@gmail.com>
2022-01-10 13:18:54 -0500

    separated video list

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -1,30 +1,6 @@
 import VideoPlayer from '../VideoPlayer'
 import styles from './styles.module.css'
-
-const VIDEOS = [
-  {
-    id: 1,
-    author: 'kevinpalmeraandrade',
-    description: '#humor #fyp #parati #ajajaja #viral #risa',
-    src: 'https://v16-webapp.tiktok.com/59318bc7da3b4ac7f84e88c59c047982/61da23e3/video/tos/useast5/tos-useast5-ve-0068c001-tx/0077cb590cf74dac8faafac704052c9a/?a=1988&br=1892&bt=946&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFkkag3-I&l=2022010817524001018907422527D0A1A8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anZ4dGc6ZmZ5OTMzZzczNEApZjRkZzxlOTw1N2Q4ODU2aWdgaGc2cjRfbXBgLS1kMS9zcy0wNDYtMTEuLi8xY2AyMDU6Yw%3D%3D&vl=&vr=',
-    likes: 21,
-    commeents: 333,
-    shares: 23,
-    songTitle: 'sonido original - Kevin Palmera Andrad',
-    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e53b3121c80bb9ce05ad0f6c3cec327~c5_720x720.jpeg?x-expires=1641747600&x-signature=D1DF0iUfQvZGycCscfohSzGAAdo%3D'
-  },
-  {
-    id: 2,
-    author: 'marcosjcj7',
-    description: 'Guerraaa de stickers 🔥 #parati #foryou',
-    src: 'https://v16-webapp.tiktok.com/e287731b348f118a2daa77f7e49313ff/61da2412/video/tos/useast2a/tos-useast2a-ve-0068c003/42001a8051244c1a81bb2599cd4736b0/?a=1988&br=1080&bt=540&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFkkag3-I&l=2022010817524001018907422527D0A1A8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajZ4ZGg6ZjN5OTMzNzczM0ApPDU7NzM0OTw8NzxmN2lpPGc0LWJocjRfXm9gLS1kMTZzczBgLTUxYy00LjMuY2MuLS06Yw%3D%3D&vl=&vr=',
-    likes: 1.6,
-    commeents: 16.5,
-    shares: 99.7,
-    songTitle: 'Vale la Pena - Live - Juan Luis Guerra 4.40',
-    albumCover: 'https://p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/cdd823001b584eb5a20c5297a0b144a3~c5_200x200.jpeg'
-  }
-]
+import { VIDEOS } from '../../constants/videos'
 
 export default function FeedVideos () {
   return (
---
+++ src/constants/videos.ts
@@ -0,0 +1,35 @@
+export const VIDEOS = [
+  {
+    id: 1,
+    author: 'kevinpalmeraandrade',
+    description: '#humor #fyp #parati #ajajaja #viral #risa',
+    src: 'https://v16-webapp.tiktok.com/59318bc7da3b4ac7f84e88c59c047982/61da23e3/video/tos/useast5/tos-useast5-ve-0068c001-tx/0077cb590cf74dac8faafac704052c9a/?a=1988&br=1892&bt=946&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFkkag3-I&l=2022010817524001018907422527D0A1A8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anZ4dGc6ZmZ5OTMzZzczNEApZjRkZzxlOTw1N2Q4ODU2aWdgaGc2cjRfbXBgLS1kMS9zcy0wNDYtMTEuLi8xY2AyMDU6Yw%3D%3D&vl=&vr=',
+    likes: 21,
+    commeents: 333,
+    shares: 23,
+    songTitle: 'sonido original - Kevin Palmera Andrad',
+    albumCover: 'https://p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/faedcfbc9cc946b09146d5ca16c321d3~c5_200x200.jpeg'
+  },
+  {
+    id: 2,
+    author: 'marcosjcj7',
+    description: 'Guerraaa de stickers 🔥 #parati #foryou',
+    src: 'https://v16-webapp.tiktok.com/e287731b348f118a2daa77f7e49313ff/61da2412/video/tos/useast2a/tos-useast2a-ve-0068c003/42001a8051244c1a81bb2599cd4736b0/?a=1988&br=1080&bt=540&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFkkag3-I&l=2022010817524001018907422527D0A1A8&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajZ4ZGg6ZjN5OTMzNzczM0ApPDU7NzM0OTw8NzxmN2lpPGc0LWJocjRfXm9gLS1kMTZzczBgLTUxYy00LjMuY2MuLS06Yw%3D%3D&vl=&vr=',
+    likes: 1.6,
+    commeents: 16.5,
+    shares: 99.7,
+    songTitle: 'Vale la Pena - Live - Juan Luis Guerra 4.40',
+    albumCover: 'https://p16-amd-va.tiktokcdn.com/img/tos-useast2a-v-2774/cdd823001b584eb5a20c5297a0b144a3~c5_200x200.jpeg'
+  },
+  {
+    id: 3,
+    author: 'annflxke',
+    description: '“rouge” ❤️‍🩹 #art #tiktokart #artistsoftiktok #fyp #parati',
+    src: 'https://v16-webapp.tiktok.com/2b5bac750fc80e14d3c68e4ce3c84e5f/61dcc499/video/tos/useast2a/tos-useast2a-ve-0068c004/531d25542c7c4e5db1698c2f38de6183/?a=1988&br=2144&bt=1072&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FFkkag3-I&l=20220110174255010223084141143A2F49&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anhzNGY6Zjh1OTMzNzczM0ApOzo8NTo6Zjs0Nzk3NDw6O2drMl42cjRnam9gLS1kMTZzczIyX2M0XzA2YTMtY2MuYF86Yw%3D%3D&vl=&vr=',
+    likes: 3.6,
+    commeents: 5151,
+    shares: 19,
+    songTitle: 'sonido original - Emanuel_MS',
+    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/51d868eca6dfc5683f2c74d501076444~c5_720x720.jpeg?x-expires=1641920400&x-signature=ZPxnpatrsua90yr9MLyH9X%2Bz2Xk%3D'
+  }
+]
