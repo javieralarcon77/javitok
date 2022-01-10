9d0161d6c55fa7ad76a4b36ced35ec04fafcca01
Javier Alarcon <alarcon.vt@gmail.com>
2022-01-10 12:41:20 -0500

    implement component video description

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -2,8 +2,9 @@ import clsx from 'clsx'
 import { useRef, useState } from 'react'
 import styles from './styles.module.css'
 import VideoPlayerActions from './VideoPlayerActions'
+import VideoDescription from '../VideoDescription/index'
 
-export default function VideoPlayer ({ src }) {
+export default function VideoPlayer ({ src, author, description, albumCover }) {
   const [playing, setPlaying] = useState(false)
   const video = useRef()
   const handlePlay = () => {
@@ -31,6 +32,11 @@ export default function VideoPlayer ({ src }) {
       />
       <i className={playerClassName} />
       <VideoPlayerActions />
+      <VideoDescription
+        author={author}
+        description={description}
+        albumCover={albumCover}
+      />
     </div>
   )
 }
